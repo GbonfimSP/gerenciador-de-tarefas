5b0f73ffedef2947063ac2a6150c00cee4d25bfd
Gabriel Bonfim <ghbonfim454@gmail.com>
2025-05-01 21:18:42 -0300

    Dia 3 - Adiciona metodos para tarefas e integração com main

--- GerenciadorDeTarefas/GerenciadorDeTarefas/Program.cs
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using GerenciadorDeTarefas;
+
+
+
+List<Tarefa> tarefas = new List<Tarefa>();
+Metodos.AdicionarTarefa(tarefas, "Estudar C#", "Estudar o básico do C#.", DateTime.Now.AddDays(2));
+Metodos.ExibirTarefass(tarefas);
+
+
+
+Metodos.AdicionarTarefa(tarefas, "Estudar C#", "Estudar o básico do C#", DateTime.Now.AddDays(2));
+Metodos.ExibirTarefass(tarefas);
+
--- GerenciadorDeTarefas/GerenciadorDeTarefas/Tarefa.cs
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Tarefa.cs
@@ -8,10 +8,35 @@ namespace GerenciadorDeTarefas
 {
     public class Tarefa
     {
-        public string Titulo { get; set; }
-        public string Descricao { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
         public DateTime DataDeConclusao { get; set; }
         public bool Concluida { get; set; }
 
+       
+    }
+
+    public class Metodos
+    {
+        public static void AdicionarTarefa(List<Tarefa> tarefas, string titulo, string descricao, DateTime dataConclusao)
+        {
+            Tarefa novaTarefa = new Tarefa()
+            {
+                Titulo = titulo,
+                Descricao = descricao,
+                DataDeConclusao = dataConclusao,
+                Concluida = false
+
+            };
+            tarefas.Add(novaTarefa);
+        }
+
+        public static void ExibirTarefass(List<Tarefa> tarefas)
+        {
+            foreach (var tarefa in tarefas)
+            {
+                Console.WriteLine($"Titulo: {tarefa.Titulo}, Concluída: {tarefa.Concluida}");
+            }
+        }
     }
 }
